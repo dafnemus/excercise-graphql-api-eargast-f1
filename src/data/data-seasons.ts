e18c4d3332a288d9d14dc7e7049af2142dbe390d
dafnemus <dafnemus@gmail.com>
2022-08-31 17:25:12 -0300

    Dafne Musante | queries - added seasonList query - connect seasonList query with dataSources - created getSeasons in SeasonsData class - identation

--- src/data/data-seasons.ts
+++ src/data/data-seasons.ts
@@ -1,7 +1,13 @@
-import { F1 } from "./data-source";
+import { F1 } from './data-source';
 
 export class SeasonsData extends F1 {
   constructor() {
     super();
+  }
+
+  async getSeasons() {
+    return await this.get('seasons.json?limit=80', {
+      cacheOptions: { ttl: 60 },
+    });
   }
 }
--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -1,8 +1,13 @@
 import { IResolvers } from 'graphql-tools';
 
 const query: IResolvers = {
-    Query: {
-    }
+  Query: {
+    async seasonsList(_: void, __: any, { dataSources }) {
+      	return await dataSources.seasons
+        .getSeasons()
+        .then((data: any) => data.MRData.SeasonTable.Seasons);
+    },
+  },
 };
 
 export default query;
