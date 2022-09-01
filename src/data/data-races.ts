821e505a9fdd4322f69d6a3a8ccb650d4a28a586
dafnemus <dafnemus@gmail.com>
2022-09-01 18:36:27 -0300

    Dafne Musante | season - race - round - created checkRound function

--- src/data/data-races.ts
+++ src/data/data-races.ts
@@ -1,3 +1,4 @@
+import { checkYear } from '../lib/utils';
 import { F1 } from './data-source';
 
 export class RacesData extends F1 {
@@ -6,13 +7,7 @@ export class RacesData extends F1 {
   }
 
   async getYear(year: string) {
-    const currentYear = new Date().getFullYear();
-    const firstYear = 1950;
-
-    if (isNaN(+year) || +year < firstYear || +year > currentYear) {
-      year = String(currentYear);
-    }
-
+    year = checkYear(year);
     return await this.get(`${year}.json`, {
       cacheOptions: { ttl: 60 },
     });
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,23 @@
 export function getWikipediaMobileUrl(url: string) {
   return url !== undefined ? url.replace('wikipedia', 'm.wikipedia') : '';
 }
+
+export function checkYear(year: string) {
+  const currentYear = new Date().getFullYear();
+  const firstYear = 1950;
+
+  if (isNaN(+year) || +year < firstYear || +year > currentYear) {
+    year = String(currentYear);
+  }
+
+  return year;
+}
+
+export function checkRound(round: number) {
+  const firstRound = 1;
+  const lastRound = 99;
+  if (round >= firstRound && round <= lastRound) {
+    return round;
+  }
+  return firstRound;
+}
