ec86a8c4e2b060ce2481104dd9023db0a07f6cc6
dafnemus <dafnemus@gmail.com>
2022-09-05 12:02:48 -0300

    Dafne Musante | One race - get one race by year and round - created receSelect type in schema - created getYearRound in RacesData class - configured raceSelect query to get one race

--- src/data/data-races.ts
+++ src/data/data-races.ts
@@ -1,4 +1,4 @@
-import { checkYear } from '../lib/utils';
+import { checkRound, checkYear } from '../lib/utils';
 import { F1 } from './data-source';
 
 export class RacesData extends F1 {
@@ -9,6 +9,14 @@ export class RacesData extends F1 {
   async getYear(year: string) {
     year = checkYear(year);
     return await this.get(`${year}.json`, {
+      cacheOptions: { ttl: 60 },
+    });
+  }
+
+  async getYearRound(year: string, round: number) {
+    year = checkYear(year);
+    round = checkRound(round);
+    return await this.get(`${year}/${round}.json`, {
       cacheOptions: { ttl: 60 },
     });
   }
--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -12,6 +12,12 @@ const query: IResolvers = {
         .getYear(year)
         .then((data: any) => data.MRData.RaceTable.Races);
     },
+
+    async raceSelect(_: void, { year, round }, { dataSources }) {
+      return await dataSources.races
+        .getYearRound(year, round)
+        .then((data: any) => data.MRData.RaceTable.Races[0]);
+    },
   },
 };
 
